0e43c3f8c2eddfb289d06a261c22d391c9f277f5
Menachem Hornbacher <mendelh1537@gmail.com>
2017-06-21 16:56:27 -0400

    basic tests begin now

--- spec/controllers/languages_controller_spec.rb
+++ spec/controllers/languages_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe LanguagesController, type: :controller do
+# RSpec.describe LanguagesController, type: :controller do
 
-end
+# end
--- spec/models/framework_spec.rb
+++ spec/models/framework_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Framework, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @rails = Framework.create(name: "Rails", description: "Convention wins!")
+    @ruby = Language.create(name: "Ruby", description: "love the self") # but have occasional breakups
+  end
+  
+  it 'has a name and description' do
+    expect(Framework.new).to respond_to(:name)
+    expect(Framework.new).to respond_to(:description)
+  end
+  
 end
