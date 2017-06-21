92a8e7e6c02559006c6e42754d1eadb9f9872ded
Menachem Hornbacher <mendelh1537@gmail.com>
2017-06-21 19:16:16 -0400

    Scopes now working :-)

--- app/models/framework.rb
+++ app/models/framework.rb
@@ -1,4 +1,13 @@
 class Framework < ActiveRecord::Base
     has_many :libraries
     has_many :languages, through: :libraries
+    has_many :reviews, through: :libraries
+
+    scope :most_popular, -> { joins(libraries: [:reviews]).group("frameworks.id").order('count("reviews.id") DESC').distinct }
+    scope :by_language, -> language { joins(libraries: [:language]).where("libraries.language_id": language).distinct }
+
+    def average_rating
+        self.reviews.average(:rating)
+    end
+    
 end
--- spec/models/framework_spec.rb
+++ spec/models/framework_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Framework, type: :model do
 
-  describe "Properties/Relationships" do
+  before(:each) do
+    @rails = Framework.create(name: "Rails", description: "Convention wins!")
+    @ruby = Language.create(name: "Ruby", description: "love the self") # but have occasional breakups
+    @devise = Library.create(name: "devise", description: "user auth taken care of", framework: @rails, language: @ruby)
+    @user = User.create(email: Faker::Internet.free_email, password: "password")
+  end
 
-    before(:each) do
-      @rails = Framework.create(name: "Rails", description: "Convention wins!")
-      @ruby = Language.create(name: "Ruby", description: "love the self") # but have occasional breakups
-    end
+  describe "Properties/Relationships" do
     
     it 'has a name and description' do
       expect(Framework.new).to respond_to(:name)
@@ -26,5 +28,53 @@ RSpec.describe Framework, type: :model do
 
   end  
   # More tests here
+
+  describe "Functions" do
+    
+    it 'can calculate average_rating' do
+      Review.create(rating: 5, user: @user, library: @devise)
+      Review.create(rating: 0, user: @user, library: @devise)
+      expect(@rails.average_rating).to eq(2.5)
+    end
+    
+  end
+  
+  describe "Scopes" do
+
+    before(:each) do
+      @python = Language.create(name: "python")
+      @django = Framework.create(name: "django")
+      @flask = Framework.create(name: "flask")
+      @django_rest = @python.libraries.create(name: "django-rest", framework: @django)
+      @django_jet = @python.libraries.create(name: "django-jet", framework: @django)
+      @flask_assets = @python.libraries.create(name: "flask-assets", framework: @flask)
+
+      @flask_assets.reviews.create(rating: 3)
+      @django_rest.reviews.create(rating: 3)
+
+      3.times do
+        @devise.reviews.create(rating: 4)
+      end
+      
+    end
+
+    it 'can get the most popular' do
+      expect(Framework.most_popular.first).to eq(@rails)
+    end
+    
+    it 'can get all frameworks for a given language' do
+      expect(Framework.by_language(@python).to_ary).to eq([@django, @flask])
+    end
+    
+    it 'can get the most popular for a given language' do
+      @django_jet.reviews.create(rating: 5)
+      expect(Framework.by_language(@python).most_popular.first).to eq(@django)
+
+      2.times { @flask_assets.reviews.create(rating: 2) }
+      expect(Framework.by_language(@python).most_popular.first).to eq(@flask)
+    end
+    
+  end
+  
 
 end
