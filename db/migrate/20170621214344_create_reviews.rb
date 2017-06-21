30b9206f92b5f2ea58670c34910ddd76c72a6c01
Menachem Hornbacher <mendelh1537@gmail.com>
2017-06-21 17:44:40 -0400

    generated review

---
+++ db/migrate/20170621214344_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :rating
+      t.belongs_to :user, index: true
+      t.belongs_to :library, index: true
+      t.text :comment
+
+      t.timestamps null: false
+    end
+    add_foreign_key :reviews, :users
+    add_foreign_key :reviews, :libraries
+  end
+end
