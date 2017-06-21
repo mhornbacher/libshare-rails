e8683d1e9e9e69bb81ef0d67fbeaa697eb6683bc
= <=>
2017-06-21 16:26:31 -0400

    First User Tests Pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621190620) do
+ActiveRecord::Schema.define(version: 20170621200237) do
+
+  create_table "frameworks", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
