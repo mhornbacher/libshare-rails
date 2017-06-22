ad6cab69f3107f127b3c9c7e7fb427c5a7981998
Menachem Hornbacher <mendelh1537@gmail.com>
2017-06-22 17:53:08 -0400

    created framework layouts

--- app/controllers/frameworks_controller.rb
+++ app/controllers/frameworks_controller.rb
@@ -1,5 +1,24 @@
 class FrameworksController < ApplicationController
+    before_action :load_framework, only: [:show, :edit, :update, :destroy]
+
     def index
         @frameworks = Framework.all
     end
+
+    def show
+    end
+
+    def new
+        @framework = Framework.new
+    end
+    
+    def edit
+    end
+    
+
+    private 
+    def load_framework
+        @framework = Framework.find(params[:id])
+    end
+    
 end
