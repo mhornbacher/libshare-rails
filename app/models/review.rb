3c7909875d991c5dceee54317366e4297e994e27
Menachem Hornbacher <mendelh1537@gmail.com>
2017-06-22 11:19:21 -0400

    added 1-5 validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :library
+
+  # validates that the rating exists and is between 1 and 5
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 5}
 end
