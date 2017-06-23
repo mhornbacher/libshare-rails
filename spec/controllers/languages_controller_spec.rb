c5046a0f7a90f6a8ac3b3690c89649679609ca1b
Menachem Hornbacher <mendelh1537@gmail.com>
2017-06-23 13:46:10 -0400

    transfered unit test from framework to controller

--- spec/controllers/languages_controller_spec.rb
+++ spec/controllers/languages_controller_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe LanguagesController, type: :controller do
+    render_views
 
+    describe '#index' do
+        before(:each) do
+            get :index
+        end
+        
+        it 'shows a list of languages' do
+            language = Language.first
+            expect(response.body).to include(language.name)
+            expect(response.body).to include(language.description)
+        end
+        
+        it 'shows each language average_rating' do
+            expect(response.body).to include("2.67")
+        end
+        
+        it 'has a a link to make a new language' do
+            expect(response.body).to have_link("Create new language", href: new_language_path)
+        end
+    end
+    
+    describe '#show' do
+        before(:each) do
+            @language = Language.first
+            get :show, id: @language.id
+        end
+
+        it 'assigns the language to an @language variable' do
+            expect(assigns(:language)).to eq(@language)
+        end
+
+        it 'shows the language title, average_rating, and description' do
+            expect(response.body).to include(@language.name)
+            expect(response.body).to include(@language.description)
+            expect(response.body).to include(@language.average_rating.to_s)
+        end
+        
+        it 'lists the frameworks use it' do
+            expect(response.body).to include("Frameworks built on")
+            expect(response.body).to include(@language.frameworks.pluck(:name)[0])
+        end
+        
+        it 'links to libraries' do
+            lib = @language.libraries.first
+            expect(response.body).to have_link(lib.name, href: library_path(lib))
+        end
+        
+        it 'has edit and delete options' do
+            expect(response.body).to have_link("Edit", edit_language_path(@language))
+            expect(response.body).to have_link("Delete", language_path(@language))
+        end
+
+    end
+    
+    describe '#new' do
+        before(:each) do
+            get :new
+        end
+
+        it 'has name and description fields' do
+            expect(response.body).to have_field("language_name")
+            expect(response.body).to have_field("language_description")
+        end
+        
+    end
+    
+    describe '#edit' do
+        before(:each) do
+            @language = Language.first
+            get :edit, id: @language.id
+        end
+
+        it 'has prepoulated name and description fields' do
+            expect(response.body).to include(@language.name)
+            expect(response.body).to include(@language.description)
+        end
+        
+    end
 end
