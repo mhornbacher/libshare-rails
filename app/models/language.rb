40819ef984a9dbc1fba92bbc2454ee92de40ab57
Menachem Hornbacher <mendelh1537@gmail.com>
2017-06-27 12:42:38 -0400

    applied fix to remaining models

--- app/models/language.rb
+++ app/models/language.rb
@@ -7,7 +7,7 @@ class Language < ActiveRecord::Base
     has_many :reviews, through: :libraries 
     # if I add a review count column I can use , counter_cache: true with order('reviews_count DESC') to inprove performance here and on frameworks (less compleax query in exchange for some writes)
 
-    scope :most_popular, -> { joins(libraries: [:reviews]).group("languages.id").order('COUNT("reviews"."id") DESC').distinct }
+    scope :most_popular, -> { select('languages.*, COUNT("reviews"."id")').joins(libraries: [:reviews]).group("languages.id").order('COUNT("reviews"."id") DESC').distinct }
     scope :by_framework, -> framework {joins(libraries: [:reviews]).where("libraries.framework_id": framework).distinct }
 
     validates :name, presence: true
