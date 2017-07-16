7d715092a5edb57bca97565bc971e320de21eca8
Menachem Hornbacher <mendelh1537@gmail.com>
2017-07-16 18:13:31 -0400

    added much needed spacing

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -12,7 +12,7 @@ Comment.prototype.renderLi = function(){
     // create and fill int the template
     var res = "<li class='review-li'><p><strong>";
     res += this.rating + " <span class='glyphicon glyphicon-star'></span> - ";
-    res += this.user_name + "</strong> at";
+    res += this.user_name + "</strong> at ";
     res += this.updated_at + "</p>";
     // show the comment if it exists
     if (this.has_comment()){
