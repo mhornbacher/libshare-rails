60d2518ce8f0a08d7f76372e229e1c38398f0ed2
Menachem Hornbacher <mendelh1537@gmail.com>
2017-07-16 16:13:50 -0400

    added `next` route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'application#homepage'
 
   get '/libraries/most_popular' => 'libraries#most_popular', as: :most_popular_libraries
+  get '/libraries/:id/next' => 'libraries#next', as: :library_next
 
   resources :libraries do
     resources :reviews, only: [:create, :destroy, :index, :new]
